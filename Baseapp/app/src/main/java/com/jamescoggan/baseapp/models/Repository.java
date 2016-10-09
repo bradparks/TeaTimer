1b2de363428009bfe17c82409b3ff94fd382a45f
Motafoca <james.coggan@mydrivesolutions.com>
2016-10-09 12:25:32 +0100

    Added realm and extended the repository object

--- Baseapp/app/src/main/java/com/jamescoggan/baseapp/models/Repository.java
+++ Baseapp/app/src/main/java/com/jamescoggan/baseapp/models/Repository.java
@@ -1,7 +1,13 @@
 package com.jamescoggan.baseapp.models;
 
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
 @SuppressWarnings({"WeakerAccess", "unused"})
-public class Repository {
+public class Repository extends RealmObject {
+
+    @PrimaryKey
+    private long id;
 
     String name;
     String fullName;
